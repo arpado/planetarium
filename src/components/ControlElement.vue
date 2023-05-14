6dd7b317a710b6de6da6f8be93a667520d3e9b66
arpado <ozsvar.arpad@gmail.com>
2023-05-14 23:04:42 +0200

    buttons added

--- src/components/ControlElement.vue
+++ src/components/ControlElement.vue
@@ -1,94 +1,129 @@
 <template>
-  <!-- data-augmented-ui="tl-clip tr-clip br-clip bl-clip both" -->
-  <div class="container">
-      <div class="rotation-container">
-              <div
-      data-augmented-ui=" bl-clip br-clip both"
-      class="rotate-up"
-      @pointerdown="press(emitAction, 'rotateCameraUp')" @pointerup="release"
-    ><span>RL</span></div>
-<!-- tl-round tr-round -->
-    <div
-      data-augmented-ui="tl-clip bl-clip both"
-      class="rotate-right"
-      @pointerdown="press(emitAction, 'rotateCameraRight')" @pointerup="release"
-    ><span>RR</span></div>
-<!-- tr-round br-round -->
-    <div
-      data-augmented-ui="tl-clip tr-clip both"
-      class="rotate-down"
-      @pointerdown="press(emitAction, 'rotateCameraDown')" @pointerup="release"
-    ><span>RD</span></div>
-    <!-- bl-round br-round -->
-    <div
-      data-augmented-ui="tr-clip br-clip both"
-      class="rotate-left"
-      @pointerdown="press(emitAction, 'rotateCameraLeft')" @pointerup="release"
-    ><span>RL</span></div>
-    <!-- tl-round bl-round -->
+  <div class="container" v-show="showNavigation">
+    <div class="rotation-container">
+      <div
+        data-augmented-ui=" bl-clip br-clip both"
+        class="rotate-up"
+        @pointerdown="press(emitAction, 'rotateCameraUp')"
+        @pointerup="release"
+      >
+        <span>RL</span>
       </div>
+      <!-- tl-round tr-round -->
+      <div
+        data-augmented-ui="tl-clip bl-clip both"
+        class="rotate-right"
+        @pointerdown="press(emitAction, 'rotateCameraRight')"
+        @pointerup="release"
+      >
+        <span>RR</span>
+      </div>
+      <!-- tr-round br-round -->
+      <div
+        data-augmented-ui="tl-clip tr-clip both"
+        class="rotate-down"
+        @pointerdown="press(emitAction, 'rotateCameraDown')"
+        @pointerup="release"
+      >
+        <span>RD</span>
+      </div>
+      <!-- bl-round br-round -->
+      <div
+        data-augmented-ui="tr-clip br-clip both"
+        class="rotate-left"
+        @pointerdown="press(emitAction, 'rotateCameraLeft')"
+        @pointerup="release"
+      >
+        <span>RL</span>
+      </div>
+      <!-- tl-round bl-round -->
+      <div
+        data-augmented-ui="tl-clip tr-clip br-clip bl-clip both"
+        class="rotate-reset"
+        @pointerdown="press(emitAction, 'restoreDefault')"
+        @pointerup="release"
+      >
+        <span>Reset</span>
+      </div>
+    </div>
+
+    <div class="zoom-container">
+      <div
+        class="zoom-in"
+        data-augmented-ui="tl-clip tr-clip br-clip bl-clip both"
+        @pointerdown="press(emitAction, 'zoomIn')"
+        @pointerup="release"
+      >
+        ZI
+      </div>
+      <div
+        class="zoom-reset"
+        data-augmented-ui="tl-clip tr-clip br-clip bl-clip both"
+        @pointerdown="press(emitAction, 'restoreDefault')"
+        @pointerup="release"
+      >
+        reset
+      </div>
+      <div
+        class="zoom-out"
+        data-augmented-ui="tl-clip tr-clip br-clip bl-clip both"
+        @pointerdown="press(emitAction, 'zoomOut')"
+        @pointerup="release"
+      >
+        ZO
+      </div>
+    </div>
 
     <div class="pan-container">
-           <div
-      data-augmented-ui="bl-clip br-clip both"
-      class="pan-up"
-      @pointerdown="press(emitAction, 'panCameraUp')" @pointerup="release"
-    >PM</div>
-    <div
-      data-augmented-ui="tl-clip bl-clip both"
-      class="pan-right"
-      @pointerdown="press(emitAction, 'panCameraRight')" @pointerup="release"
-    >PR</div>
-        <div
-      data-augmented-ui="tl-clip tr-clip both"
-      class="pan-down"
-      @pointerdown="press(emitAction, 'panCameraDown')" @pointerup="release"
-    >PD</div>
-    <div
-      data-augmented-ui="tr-clip br-clip both"
-      class="pan-left"
-      @pointerdown="press(emitAction, 'panCameraLeft')" @pointerup="release"
-    ><span>Move Left</span></div>
+      <div
+        data-augmented-ui="bl-clip br-clip both"
+        class="pan-up"
+        @pointerdown="press(emitAction, 'panCameraUp')"
+        @pointerup="release"
+      >
+        PU
+      </div>
+      <div
+        data-augmented-ui="tl-clip bl-clip both"
+        class="pan-right"
+        @pointerdown="press(emitAction, 'panCameraRight')"
+        @pointerup="release"
+      >
+        PR
+      </div>
+      <div
+        data-augmented-ui="tl-clip tr-clip both"
+        class="pan-down"
+        @pointerdown="press(emitAction, 'panCameraDown')"
+        @pointerup="release"
+      >
+        PD
+      </div>
+      <div
+        data-augmented-ui="tr-clip br-clip both"
+        class="pan-left"
+        @pointerdown="press(emitAction, 'panCameraLeft')"
+        @pointerup="release"
+      >
+        <span>PL</span>
+      </div>
+      <div
+        data-augmented-ui="tl-clip tr-clip br-clip bl-clip both"
+        class="rotate-reset"
+        @pointerdown="press(emitAction, 'restoreDefault')"
+        @pointerup="release"
+      >
+        <span>Reset</span>
+      </div>
     </div>
- 
-
-    <!-- <button >
-      
-    </button>
-    <button >
-      Move Right
-    </button>
-    <button >
-      Move Up
-    </button>
-    <button >
-      Move Down
-    </button>
-    <button >
-      Pan Left
-    </button>
-    <button >
-      Pan Right
-    </button>
-    <button >
-      Pan Up
-    </button>
-    <button >
-      Pan Down
-    </button>
-    <button @pointerdown="press(emitAction, 'zoomIn')" @pointerup="release">
-      Zoom In
-    </button>    
-    <button @pointerdown="press(emitAction, 'zoomOut')" @pointerup="release">
-      Zoom Out
-    </button>
-    <button @pointerdown="press(emitAction, 'restoreDefault')" @pointerup="release">
-      Restore Default
-    </button> -->
+  </div>
+  <div class="panel-toggler" data-augmented-ui="tl-clip tr-clip br-clip bl-clip both" @click="toggleNavigation">
+      toggle panel
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 const emit = defineEmits([
   'rotateCameraLeft',
   'rotateCameraRight',
@@ -114,11 +149,15 @@ function release() {
 function emitAction(action) {
   emit(action)
 }
+let showNavigation = ref(true)
+function toggleNavigation() {
+    showNavigation.value = !showNavigation.value
+}
 </script>
 
 <style scoped>
 .container {
-  width: 100%;
+  width: 90vw;
   height: 200px;
   /* border-radius: 50%; */
   position: absolute;
@@ -129,9 +168,10 @@ function emitAction(action) {
   color: white;
   /* --aug-tl: 50%;
   --aug-bl: 50%; */
-  border: 1px solid white;
-    display: flex;
-    justify-content: space-between;
+  /* border: 1px solid white; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .container,
 .container::after,
@@ -147,13 +187,14 @@ function emitAction(action) {
   width: 200px;
   height: 200px;
 }
+
 .rotate-up,
 .pan-up {
-    grid-column: 1/7;
-    grid-row: 1/2;
-    /* --aug-tl: 50%;
+  grid-column: 1/7;
+  grid-row: 1/2;
+  /* --aug-tl: 50%;
   --aug-tr: 50%; */
-      /* --aug-bl: 50px;
+  /* --aug-bl: 50px;
     --aug-br: 50px; */
 }
 .rotate-right,
@@ -163,48 +204,76 @@ function emitAction(action) {
 }
 .rotate-down,
 .pan-down {
-    grid-column: 1/4;
-    grid-row: 3/4;
+  grid-column: 1/4;
+  grid-row: 3/4;
 }
 .rotate-left,
 .pan-left {
   grid-row: 1/4;
   grid-column: 1/2;
 }
-
-.rotate-up, .rotate-right, .rotate-down, .rotate-left,
-.pan-up, .pan-right, .pan-down, .pan-left {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    --aug-tl: 70px;
-    --aug-tr: 70px;
-    --aug-bl: 70px;
-    --aug-br: 70px;
-}
-/* .pan-left {
-  grid-row: 2/6;
-  grid-column: 2/3; 
-} */
-/* .pan-right {
-  grid-row: 2/6;
-  grid-column: 5/6; 
-}
-.pan-up {
+.rotate-reset {
   grid-row: 2/3;
-  grid-column: 2/6; 
+  grid-column: 2/3;
 }
-.pan-down {
-  grid-row: 5/6;
-  grid-column: 2/6; 
-} */
-.zoom-in {
+
+.rotate-up,
+.rotate-right,
+.rotate-down,
+.rotate-left,
+.pan-up,
+.pan-right,
+.pan-down,
+.pan-left {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  --aug-tl: 70px;
+  --aug-tr: 70px;
+  --aug-bl: 70px;
+  --aug-br: 70px;
 }
-.zoom-out {
+.rotate-reset {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 }
-.reset {
+.zoom-container {
+  display: flex;
+  width: fit-content;
+  height: fit-content;
 }
-.hide {
+.zoom-in,
+.zoom-out,
+.zoom-reset {
+  width: 100px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  --aug-tl: 15px;
+  --aug-tr: 15px;
+  --aug-bl: 15px;
+  --aug-br: 15px;
+}
+.panel-toggler {
+  position: absolute;
+  bottom: 25px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 200px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  --aug-tl: 15px;
+  --aug-tr: 15px;
+  --aug-bl: 15px;
+  --aug-br: 15px;
+  color: wheat;
 }
 </style>
