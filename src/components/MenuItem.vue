f31c5b6e6d245f574ade261015547ff27a626c19
arpado <ozsvar.arpad@gmail.com>
2023-05-14 23:08:00 +0200

    inputfield added, emits added

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,29 +1,43 @@
 <template>
+<div class="flex gap-4 items-center">
+  <div v-if="button.id === 'search-btn' && mainStore.showSearchBar">
+    <input type="text" name="" id="" placeholder="Find..."> 
+  </div>
   <a
     class="wrapper nav-btn"
-    :href="button.href"
+    @click="$emit(button.func)"
     data-augmented-ui="
   tl-clip tr-clip br-clip bl-clip both"
-    :style="{ right: `${btnArrayLength - index * 110 + 405}px` }"
   >
+    <!-- :style="{ right: `${btnArrayLength - index * 110 + 405}px` }" -->
     <!-- <p>{{ button.name }}</p> -->
     <Icon :icon="button.icon" />
   </a>
+
+</div>
+
 </template>
 
 <script>
 import { Icon } from '@iconify/vue';
+import { useMainStore } from '@/stores/mainStore.js'
+
 
 export default {
   name: "MenuItem",
   props: ["button", "index", "btnArrayLength"],
   components: { Icon },
+  setup() {
+    const mainStore = useMainStore()
+    return { mainStore }
+  },
+  // emits: [this.button.func]
 };
 </script>
 
 <style scoped>
 .wrapper {
-  position: absolute;
+  /* position: absolute; */
   top: 0;
   text-decoration: none;
   color: white;
@@ -37,5 +51,10 @@ export default {
 }
 a {
   font-size: 3rem;
+}
+.nav-btn {
+  --aug-inlay-opacity: 0.9;
+  --aug-inlay-bg: black;
+  --aug-inlay-all: 0px;
 }
 </style>
