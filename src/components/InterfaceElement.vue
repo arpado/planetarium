1fb37ae95257a18efd5ceceab698ceaebfef36e1
arpado <ozsvar.arpad@gmail.com>
2023-05-18 14:17:46 +0200

    component redone

--- src/components/InterfaceElement.vue
+++ src/components/InterfaceElement.vue
@@ -1,85 +1,51 @@
 <template>
-  <div class="container" v-if="mainStore.showPlanetCards">
-    <Swiper
-    :direction="'vertical'"
-    :slides-per-view="3"
-    :pagination="{
-      clickable: true,
-    }"
-    class="swiper-container swiper"
-    :modules="modules"
-    :mousewheel="true"
-  >
-    <SwiperSlide
-      v-for="(planet, index) in planetStore.planetList"
-      :key="index"
-      data-augmented-ui="tl-clip tr-clip br-clip bl-clip both"
-      class="planet-container"
-    >
-      <span>{{ planet.name }}</span>
-      <!-- <button @click="planet.value = !planet.value">{{ planet.value? 'close' : 'open' }}</button> -->
-      <button @click="planet.value = !planet.value">open</button>
-      <!-- <div :class="[{ invisible: !planet.value }, {visible: planet.value }]"> -->
-      <div v-if="planet.value">
-        <div class="flex justify-between items-center">
-          <label :for="`planet-scale-${index}`">Size</label>
-          <input
-            class="w-10 h-fit"
-            :id="`planet-scale-${index}`"
-            type="number"
-            step=".1"
-            v-model="planet.size.scale"
-          />
+  <VueDragResize class="drag-wrap" :x="getX" :y="110" v-if="mainStore.showPlanetCards">
+      <div
+        class="star-container flex flex-col items-end"
+        data-augmented-ui="tl-clip tr-clip br-clip bl-clip border"
+      >
+        <div class="star" data-augmented-ui="tl-clip tr-clip br-clip bl-clip both">
+          <span>Sun</span>
         </div>
-        <div class="flex justify-between items-center">
-          <label :for="`planet-distance-${index}`">Distance from star</label>
-          <input
-            class="w-10 h-fit"
-            :id="`planet-distance-${index}`"
-            type="number"
-            v-model="planet.radius"
-          />
-        </div>
-        <div class="flex justify-between items-center">
-          <label :for="`planet-rotation-offset-${index}`">Orbital rotation offset</label>
-          <input
-            class="w-10 h-fit"
-            :id="`planet-rotation-offset-${index}`"
-            type="number"
-            v-model="planet.rotation"
-            min="0"
-            max="360"
-          />
+
+        <div class="planet-container">
+          <div
+            v-for="(planet, index) in planetStore.planetList"
+            :key="index"
+            @click="planet.value = !planet.value"
+            class="planet-container-item flex flex-col items-end"
+            data-augmented-ui="tl-clip tr-clip br-clip bl-clip border"
+          >
+            <div class="planet" data-augmented-ui="tl-clip tr-clip br-clip bl-clip both">
+              <span>{{ planet.name }}</span>
+              <SetupElement :item="planet" :index="index" v-if="planet.value" />
+            </div>
+
+            <div
+              v-for="(moon, index) in planet.moons"
+              :key="index"
+              class="moon"
+              data-augmented-ui="tl-clip tr-clip br-clip bl-clip both"
+              @click="moon.value = !moon.value"
+            >
+              <span>{{ moon.name }}</span>
+              <SetupElement :item="moon" :index="index" v-if="moon.value" />
+            </div>
+          </div>
         </div>
       </div>
-    </SwiperSlide>
-    <button @click="planetStore.addPlanet">new planet</button>
-  </Swiper>
-  </div>
+  </VueDragResize>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+import VueDragResize from 'vue-drag-resize/src/component/vue-drag-resize.vue'
+
+import SetupElement from './SetupElement.vue'
+
 import { usePlanetStore } from '@/stores/planetStore.js'
 import { useMainStore } from '@/stores/mainStore.js'
-
-  // core version + navigation, pagination modules:
-  import { Swiper, SwiperSlide } from 'swiper/vue';
-  import { Mousewheel } from 'swiper';
-
-  // Navigation, Pagination
-  // import Swiper and modules styles
-  import 'swiper/css';
-  // import 'swiper/css/navigation';
-  // import 'swiper/css/pagination';
-
-  // init Swiper:
-  // const swiper = new Swiper('.swiper', {
-  //   // configure Swiper to use modules
-  //   modules: [Mousewheel],
-  // });
-  const modules = [Mousewheel]
-
 // access the `store` variable anywhere in the component ✨
 const planetStore = usePlanetStore()
 const mainStore = useMainStore()
@@ -88,43 +54,72 @@ const elements = ref([])
 const addElement = () => {
   elements.value.push(ref(false))
 }
+const { width, height } = useWindowSize()
+const getX = computed(() => {
+  return width.value - 250
+})
 </script>
 
 <style scoped>
-.container {
-  position: absolute;
-  top: 110px;
-  right: 10px;
-  width: 200px;
-  height: calc(100vh - 350px);
-  /* overflow-y: scroll; */
-  /* background: rgb(221, 217, 213); */
-  /* padding: 1rem; */
+.star-container-item,
+.planet-container-item {
+  --aug-inlay-all: 0px;
+  margin-bottom: 0.5rem;
+  width: fit-content;
 }
 .planet-container {
+  width: 100%;
+  display: flex;
+  flex-flow: wrap;
+  align-items: flex-start;
+  justify-content: flex-end;
+}
+.drag-wrap{
+  color: white;
+  overflow-y: scroll;
+}
+.star {
+  width: 100%;
+  height: 50px;
+  color: white;
+  margin-bottom: 0.5rem;
   padding: 1rem;
-  margin: 0.5rem;
+}
+.planet {
+  padding: 1rem;
+  /* margin: 0.5rem 0; */
   display: flex;
   flex-direction: column;
   color: white;
   /* width: 100px;
   height: 100px; */
   height: fit-content;
+  width: 165px;
+  min-height: 50px;
+  /* display: flex;
+  flex-direction: column;
+  align-items: flex-end; */
 }
-input {
-  color: black;
+.moon {
+  padding: 1rem;
+  margin: 0.5rem 0;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  /* width: 100px;
+  height: 100px; */
+  height: fit-content;
+  width: 105px;
+  min-height: 50px;
+  /* display: flex;
+  flex-direction: column;
+  align-items: flex-end; */
 }
-button {
-  margin: 10px auto;
-}
+
 .invisible {
-    height: 0;
+  height: 0;
 }
 .visible {
-    height: fit-content;
-}
-.swiper-container {
-  height: 100%;
-  width: 100%;
+  height: fit-content;
 }
 </style>
